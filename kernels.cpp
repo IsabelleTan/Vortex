5d5e7bcbf45a7b92c82f7f4b1d2acf1727209ae9
IsabelleTan <isabelle.w.tan@gmail.com>
2016-07-27 10:42:33 +0200

    Added kernels.cpp and kernels.h containing the kernel signatures for the p2e, e2p and p2p kernels.

---
+++ kernels.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Isabelle Tan on 27-07-16.
+//
+
+#include "kernels.h"
+
+/*
+ * The particle to expansion kernel. This function computes the alpha_k coefficients and stores their real and imaginary
+ * parts in the arrays rexpansion and iexpansion.
+ */
+void p2e(const value_type* const x, const value_type* const y, const value_type* const q, const int nsources, const int order, const value_type xcom, const value_type ycom, value_type* const rexpansion, value_type* const iexpansion){
+    //TODO complete p2e
+};
+
+
+/*
+ * The expansion to target particle kernel. This function computes the potential (streamfunction) value at the target particle located
+ * at xtarget, ytarget.
+ */
+value_type e2p(const double xtarget, const double ytarget, const double q, const int order, const double* const rxps, const double* const ixps){
+    // TODO complete e2p
+
+    return 0;
+};
+
+/*
+ * The particle to particle kernel. This function computes the interactions between nsources source particles and one target particle.
+ */
+value_type p2p(const value_type* const xsources, const value_type* const ysources, const value_type* const q, const int nsources, const value_type xtarget, const value_type ytarget){
+    // TODO complete p2p
+
+    return 0;
+};
---
+++ kernels.h
@@ -0,0 +1,30 @@
+//
+// Created by Isabelle Tan on 27-07-16.
+//
+
+#ifndef VORTEX_KERNELS_H
+#define VORTEX_KERNELS_H
+
+// Set the value_type
+typedef double value_type;
+
+/*
+ * The particle to expansion kernel. This function computes the alpha_k coefficients and stores their real and imaginary
+ * parts in the arrays rexpansion and iexpansion.
+ */
+void p2e(const value_type* const x, const value_type* const y, const value_type* const q, const int nsources, const int order, const value_type xcom, const value_type ycom, value_type* const rexpansion, value_type* const iexpansion);
+
+
+/*
+ * The expansion to target particle kernel. This function computes the potential (streamfunction) value at the target particle located
+ * at xtarget, ytarget.
+ */
+value_type e2p(const double xtarget, const double ytarget, const double q, const int order, const double* const rxps, const double* const ixps);
+
+/*
+ * The particle to particle kernel. This function computes the interactions between nsources source particles and one target particle.
+ */
+value_type p2p(const value_type* const xsources, const value_type* const ysources, const value_type* const q, const int nsources, const value_type xtarget, const value_type ytarget);
+
+
+#endif //VORTEX_KERNELS_H
